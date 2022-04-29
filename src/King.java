5e89c9bb207be48f6f5dd46babe9aeff15329bf6
HenryTsay <>
2022-04-29 04:09:05 +0000

    added getLegalMoves

--- src/King.java
+++ src/King.java
@@ -1,9 +1,30 @@
 package src;
+import java.util.*;
 
 public class King extends Piece{
-    public King(boolean isWhite, int rank, int file){
-        this.isWhite = isWhite;
-        this.rank = rank;
-        this.file = file;
+  public King(boolean isWhite, int rank, int file){
+    this.isWhite = isWhite;
+    this.rank = rank;
+    this.file = file;
+  }
+
+  //doesnt account for checks yet
+  public List<Piece> getLegalMoves(Board board){
+    List<Piece> ans = new ArrayList<>();
+    for(int i=-1;i<2;i++){
+      for(int j=-1;j<2;j++){
+        if((i==0 && j == 0) || rank+i < 0 || rank+i > 7 || file+j < 0 || file+j > 7){
+          continue;
+        }
+        Square s = board[rank+i][file+j];
+        if(s.getPiece().isWhite() == isWhite){
+          continue;
+        }
+        ans.add(s);
       }
+    }
+    return ans;
+  }
+
+  
 }
--- src/Piece.java
+++ src/Piece.java
@@ -3,4 +3,7 @@ package src;
 public abstract class Piece{
   protected boolean isWhite;
   protected int rank,file;
+
+  //returns a list of legal squares it can move to
+  public abstract List<Piece> getLegalMoves(Board board);
 }
