fa3fa8136bb11d2bd215e9afa557df3d530e1810
unknown <ht4411@pleasantonusd.net>
2022-05-03 21:32:57 -0700

    added getLegalMoves in Rook and Queen classes

--- src/Queen.java
+++ src/Queen.java
@@ -1,14 +1,138 @@
 package src;
+
 import java.util.*;
 
-public class Queen extends Piece{
-    public Queen(boolean isWhite, int rank, int file){
+public class Queen extends Piece {
+    public Queen(boolean isWhite, int rank, int file) {
         this.isWhite = isWhite;
         this.rank = rank;
         this.file = file;
     }
 
-    public ArrayList<Square> getLegalMoves(Board board) {
-        return null;
+    // doesn't account for checks
+    public ArrayList<Square> getLegalMoves(Board b) {
+        Square[][] board = b.getGrid();
+        ArrayList<Square> ans = new ArrayList<>();
+
+        // top left
+        int r = rank;
+        int f = file;
+        while (r >= 0 && f >= 0) {
+            r--;
+            f--;
+            // check if piece is in the way, then checks color
+            if (board[r][f].hasPiece()) {
+                if (board[r][f].getPiece().isWhite() != this.isWhite) {
+                    ans.add(board[r][f]);
+                    break;
+                }
+            }
+            ans.add(board[r][f]);
+        }
+
+        // top right
+        r = rank;
+        f = file;
+        while (r >= 0 && f < 8) {
+            r--;
+            f++;
+            if (board[r][f].hasPiece()) {
+                if (board[r][f].getPiece().isWhite() != this.isWhite) {
+                    ans.add(board[r][f]);
+                    break;
+                }
+            }
+            ans.add(board[r][f]);
+        }
+
+        // bottom left
+        r = rank;
+        f = file;
+        while (r < 8 && f >= 0) {
+            r++;
+            f--;
+            if (board[r][f].hasPiece()) {
+                if (board[r][f].getPiece().isWhite() != this.isWhite) {
+                    ans.add(board[r][f]);
+                    break;
+                }
+            }
+            ans.add(board[r][f]);
+        }
+
+        // bottom right
+        r = rank;
+        f = file;
+        while (r < 8 && f < 8) {
+            r++;
+            f++;
+            if (board[r][f].hasPiece()) {
+                if (board[r][f].getPiece().isWhite() != this.isWhite) {
+                    ans.add(board[r][f]);
+                    break;
+                }
+            }
+            ans.add(board[r][f]);
+        }
+
+        // up
+        r = rank;
+        f = file;
+        while (r >= 0) {
+            r--;
+            // check if piece is in the way, then checks color
+            if (board[r][f].hasPiece()) {
+                if (board[r][f].getPiece().isWhite() != this.isWhite) {
+                    ans.add(board[r][f]);
+                    break;
+                }
+            }
+            ans.add(board[r][f]);
+        }
+
+        // right
+        r = rank;
+        f = file;
+        while (f < 8) {
+            f++;
+            if (board[r][f].hasPiece()) {
+                if (board[r][f].getPiece().isWhite() != this.isWhite) {
+                    ans.add(board[r][f]);
+                    break;
+                }
+            }
+            ans.add(board[r][f]);
+        }
+
+        // down
+        r = rank;
+        f = file;
+        while (r < 8) {
+            r++;
+            if (board[r][f].hasPiece()) {
+                if (board[r][f].getPiece().isWhite() != this.isWhite) {
+                    ans.add(board[r][f]);
+                    break;
+                }
+            }
+            ans.add(board[r][f]);
+        }
+
+        // left
+        r = rank;
+        f = file;
+        while (f >= 0) {
+            f--;
+            // check if piece is in the way, then checks color
+            if (board[r][f].hasPiece()) {
+                if (board[r][f].getPiece().isWhite() != this.isWhite) {
+                    ans.add(board[r][f]);
+                    break;
+                }
+            }
+            ans.add(board[r][f]);
+        }
+
+        return ans;
     }
 }
--- src/Rook.java
+++ src/Rook.java
@@ -1,14 +1,77 @@
 package src;
+
 import java.util.*;
 
-public class Rook extends Piece{
-    public Rook(boolean isWhite, int rank, int file){
+public class Rook extends Piece {
+    public Rook(boolean isWhite, int rank, int file) {
         this.isWhite = isWhite;
         this.rank = rank;
         this.file = file;
     }
 
-    public ArrayList<Square> getLegalMoves(Board board) {
-        return null;
+    // doesn't account for checks
+    public ArrayList<Square> getLegalMoves(Board b) {
+        Square[][] board = b.getGrid();
+        ArrayList<Square> ans = new ArrayList<>();
+
+        // up
+        int r = rank;
+        int f = file;
+        while (r >= 0) {
+            r--;
+            // check if piece is in the way, then checks color
+            if (board[r][f].hasPiece()) {
+                if (board[r][f].getPiece().isWhite() != this.isWhite) {
+                    ans.add(board[r][f]);
+                    break;
+                }
+            }
+            ans.add(board[r][f]);
+        }
+
+        // right
+        r = rank;
+        f = file;
+        while (f < 8) {
+            f++;
+            if (board[r][f].hasPiece()) {
+                if (board[r][f].getPiece().isWhite() != this.isWhite) {
+                    ans.add(board[r][f]);
+                    break;
+                }
+            }
+            ans.add(board[r][f]);
+        }
+
+        // down
+        r = rank;
+        f = file;
+        while (r < 8) {
+            r++;
+            if (board[r][f].hasPiece()) {
+                if (board[r][f].getPiece().isWhite() != this.isWhite) {
+                    ans.add(board[r][f]);
+                    break;
+                }
+            }
+            ans.add(board[r][f]);
+        }
+
+        // left
+        r = rank;
+        f = file;
+        while (f >= 0) {
+            f--;
+            // check if piece is in the way, then checks color
+            if (board[r][f].hasPiece()) {
+                if (board[r][f].getPiece().isWhite() != this.isWhite) {
+                    ans.add(board[r][f]);
+                    break;
+                }
+            }
+            ans.add(board[r][f]);
+        }
+
+        return ans;
     }
 }
